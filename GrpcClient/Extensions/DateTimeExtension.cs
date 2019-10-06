3307ae8c7f25ae54dda04894201fa58ade7f208b
supershowwei <supershowwei@gmail.com>
2019-10-06 12:34:27 +0800

    complete

---
+++ GrpcClient/Extensions/DateTimeExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GrpcClient.Extensions
+{
+    internal static class DateTimeExtension
+    {
+        private static readonly System.DateTime InitialTime = new System.DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static long ToUnixTimeMilliseconds(this System.DateTime me)
+        {
+            return Convert.ToInt64(me.ToUniversalTime().Subtract(InitialTime).TotalMilliseconds);
+        }
+
+        public static System.DateTime ToDateTime(this long me)
+        {
+            return InitialTime.AddMilliseconds(me).ToLocalTime();
+        }
+
+    }
+}
